1ebe8f58ceeeb9e34aeedd8e0e76200d067b5cd2
Pablo Martinez <pablodmv@gmail.com>
2018-10-17 17:35:19 -0300

    Create Book

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/mongoose
+https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/forms
--- app.js
+++ app.js
@@ -18,9 +18,10 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 
 
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var catalogRouter = require('./routes/catalog');  //Import routes for "catalog" area of site
+
 
 var app = express();
 
@@ -36,6 +37,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);  // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
